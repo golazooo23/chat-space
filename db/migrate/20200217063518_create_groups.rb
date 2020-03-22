426618525f3c9feadc3aaaad56115e6b36df8316
golazooo23 <kengoteshima0823@gmail.com>
2020-03-22 14:08:29 +0900

    Update 20200217063518_create_groups.rb

--- db/migrate/20200217063518_create_groups.rb
+++ db/migrate/20200217063518_create_groups.rb
@@ -1,8 +1,13 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
+      # NOT NULL制約 nameはカラム名,stringはデータ型
       t.string :name, null: false
-      t.index :name, unique: true
+      # 一意性制約 インデックス必須 usersはテーブル名、emailはカラム名、stringはデータ型
+      add_column :users, :email, :string
+      add_index :users, :email, unique: true
+      # 外部キー referencesはデータ型 これでuser_idというカラムが生成される
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
